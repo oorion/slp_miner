96d2a724405918416a7456ad7d0ee5b1098fe316
Orion Osborn <orion.osborn@gmail.com>
2019-09-28 11:55:43 -0600

    add basic inital form

--- src/App.js
+++ src/App.js
@@ -6,18 +6,44 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>SLP Miner</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form>
+          <label class="label">
+            Name
+            <input type="text" name="name" />
+          </label>
+          <label class="label">
+            Symbol
+            <input type="text" name="symbol" />
+          </label>
+          <label class="label">
+            Total Token Quantity
+            <input type="text" name="initialTokenQuantity" />
+          </label>
+          <label class="label">
+            Amount of tokens to mint each interval
+            <input type="text" name="intervalTokenAmount" />
+          </label>
+          <label class="label">
+            Minting Time Interval
+            <input type="text" name="interval" />
+          </label>
+          <label class="label">
+            Decimals
+            <input type="text" name="decimals" />
+          </label>
+          <label class="label">
+            Document URL
+            <input type="text" name="documentURL" />
+          </label>
+          <label class="label">
+            Document Hash
+            <input type="text" name="documentHash" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
